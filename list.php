c727614c98236695fec9b9b865de5b4c2559df3a
Timo Varila <timo.varila@haaga-helia.fi>
2019-02-05 16:01:21 +0200

    list addresses

--- list.php
+++ list.php
@@ -1,19 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Using jquery</title>  
-  <meta charset="utf-8">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-</head>
-<body>
-<h1>List</h1>
+<?php
+session_start();
+if($_SESSION['username'] == 'username') {
+ include "connect.php";
+ print("<html>");
+ $conn = connect_db();
+ $sql = "SELECT * FROM Addresses";
+ $result = mysqli_query($conn, $sql);
+ if (mysqli_num_rows($result) > 0) {
+ // output data of each row
+ while($row = mysqli_fetch_assoc($result)) {
+ echo $row["id"] . " ";
+ echo $row["name"] . " ";
+ echo $row["street_address"] . " ";
+ echo $row["zip"] . " ";
+ echo $row["city"] . " ";
+ echo $row["state"] . " ";
+ echo $row["country"] . " ";
+ print("<br>");
+ }
+ } else {
+ echo "0 results";
+ }
+ print("</html>");
+ mysqli_close($conn);
+} else {
+ print("No access.");
+}
+?>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script>
-  $(document).ready(function() {
-    // Insert your code here
-  });
-</script>
-</body>
-</html>
